75ef22af905f0fb7b3171657bc5248aa76f2c9e3
zmasud1 <zmasud@wesleyan.edu>
2025-09-18 11:35:20 -0400

    Update assignment2-zm.r
    Added frequency table for year

--- assignment2-zm.r
+++ assignment2-zm.r
@@ -1,7 +1,9 @@
 library(readr)
-penguins = read_csv("Documents/GitHub/assignment2-zm/penguins.csv")
+penguins = read_csv("~/Documents/GitHub/assignment2-zm/penguins.csv")
 View(penguins)
 
 table(penguins$species)
 
 table(penguins$island)
+
+table(penguins$year)
