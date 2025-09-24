e5ff4230e3f29849b53119b0540fe0f5c65fdd9f
zmasud1 <zmasud@wesleyan.edu>
2025-09-24 17:40:11 -0400

    Commented output
    added output as comments

--- assignment2-zm.r
+++ assignment2-zm.r
@@ -4,6 +4,16 @@ View(penguins)
 
 table(penguins$species)
 
+#Adelie Chinstrap    Gentoo 
+#152        68       124 
+
+
 table(penguins$island)
+#Biscoe     Dream Torgersen 
+#168       124        52 
+
 
 table(penguins$year)
+
+#2007 2008 2009 
+#110  114  120 
